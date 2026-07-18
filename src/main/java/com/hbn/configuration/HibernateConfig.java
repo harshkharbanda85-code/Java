eb835a0cfdfdeb542d6238b415ce3283cb52dde9
harshkharbanda85-code <harshkharbanda85@gmail.com>
2026-07-18 14:12:03 +0530

    My first commit of git

---
+++ src/main/java/com/hbn/configuration/HibernateConfig.java
@@ -0,0 +1,41 @@
+package com.hbn.configuration;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Environment;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//import com.hbn.entity.Employee;
+
+public class HibernateConfig {
+	
+
+//	public static Object getSessionFactory;
+
+	public static SessionFactory getSessionFactory()
+	{
+		
+		Properties properties = new Properties();
+		
+		properties.put("hibernate.dialect","org.hibernate.dialect.MySQLDialect" );
+		properties.put("hibernate.connection.driver_class","com.mysql.cj.jdbc.Driver");
+		properties.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://localhost:3306/employee");
+		properties.put(Environment.JAKARTA_JDBC_USER, "root");
+		properties.put(Environment.JAKARTA_JDBC_PASSWORD, "Harsh@12");
+		properties.put(Environment.HBM2DDL_AUTO, "update");
+		properties.put(Environment.SHOW_SQL, "true");
+		properties.put(Environment.FORMAT_SQL, "true");
+		
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(properties).build();
+		
+		Metadata meta = new MetadataSources(ssr).addAnnotatedClass(com.hbn.entity.Employee.class).getMetadataBuilder().build();
+		
+		return meta.buildSessionFactory();
+		
+		
+	}
+
+}
